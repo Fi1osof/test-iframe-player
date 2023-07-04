b20e8fa9398e08796be6b297a768a9589df45a9f
Fi1osof <n.lanets@freecode.academy>
2023-07-04 09:35:20 +0300

    Fix github pages deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,62 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // basePath: '/test-iframe-player.github.io',
+};
+
+/**
+ * Github pages
+ */
+if (
+  process.env.GITHUB_REPOSITORY &&
+  ["phase-production-build", "phase-export"].includes(phase)
+) {
+  const repositoryName = process.env.GITHUB_REPOSITORY.split("/")[1];
+
+  nextConfig = {
+    ...nextConfig,
+    assetPrefix: `/${repositoryName}/`,
+    basePath: `/${repositoryName}`,
+  };
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// const webpack = (config, options) => {
+
+//   return config
+// }
+
+// module.exports = (phase, defaultConfig) => {
+//   if (phase !== 'phase-production-server') {
+//     const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//       enabled: process.env.ANALYZE === 'true',
+//     })
+
+//     /**
+//      * Github pages
+//      */
+//     if (
+//       process.env.GITHUB_REPOSITORY &&
+//       ['phase-production-build', 'phase-export'].includes(phase)
+//     ) {
+//       const repositoryName = process.env.GITHUB_REPOSITORY.split('/')[1]
+
+//       config = {
+//         ...config,
+//         assetPrefix: `/${repositoryName}/`,
+//         basePath: `/${repositoryName}`,
+//       }
+//     }
+
+//     return config
+//   }
+
+//   // else
+//   // return defaultConfig
+
+//   return {
+//     ...defaultConfig,
+//     webpack,
+//     generateEtags: false,
+//   }
+// }
